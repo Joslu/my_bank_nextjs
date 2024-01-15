4d5a3589ded75ced3e9bd9f273ed797e27f2a73c
joslu <joslu.dev@gmail.com>
2024-01-15 08:04:33 -0700

    update cors

--- next.config.js
+++ next.config.js
@@ -8,5 +8,49 @@ module.exports = {
         },
       ];
     },
+
+    async headers() {
+      return [
+        {
+          // Matching specific API routes
+          source: "/login",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "https://18.222.51.51" }, // Cambia a tu dominio de API
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        },
+        {
+          // Matching specific API routes
+          source: "/logout",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "https://18.222.51.51" }, // Cambia a tu dominio de API
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        },
+        {
+          // Matching specific API routes
+          source: "/get-data",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "https://18.222.51.51" }, // Cambia a tu dominio de API
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }, {
+          // Matching specific API routes
+          source: "/create-user",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "https://18.222.51.51" }, // Cambia a tu dominio de API
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        },
+      ]
+    }
   };
  
