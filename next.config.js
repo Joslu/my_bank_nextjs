add3a1b4d4bfda15f90d2643f9b8d8116a353b91
joslu <joslu.dev@gmail.com>
2024-01-15 01:31:38 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
 
-  // Definir redirección para la ruta de inicio
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: true,
-      },
-    ];
-  },
 
-  // Otras opciones opcionales...
-};
-
-module.exports = nextConfig;
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/login',
+          permanent: true,
+        },
+      ];
+    },
+  };
+ 
